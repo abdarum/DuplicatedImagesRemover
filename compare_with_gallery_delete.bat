40247aa471a4b68b01d565fb47caa159e4909cc1
abdarum <kornelstefanczyk@wp.pl>
2022-12-21 20:56:01 +0100

    Initial comit. Moved files from https://github.com/abdarum/Python

---
+++ compare_with_gallery_delete.bat
@@ -0,0 +1,3 @@
+@REM python <path_to_repo_dir>\src\duplicated_photos_remove.py -v -p -r
+@REM python <path_to_repo_dir>\src\duplicated_photos_remove.py -P <abs_path_to_dir_with_config>\\config_file.json 
+pause
